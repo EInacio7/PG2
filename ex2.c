98267d0285dbb12236b593b9a7359731a558c10a
FranciscoFicalho97 <fbficalho@gmail.com>
2021-05-11 23:59:58 +0100

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -3,30 +3,42 @@
 #include <string.h>
 #include <stdlib.h>
 
-//abcd.c
+// && str+1 == NULL
 
 int findTerm( char *str, char *terms[], int numTerms ){
 	char *token;
+	char p = '.';
+	char *aux = malloc(sizeof(char*));
+	
+	token = strtok(str, &p);
+	strcpy(aux,token);
+	token = strtok(NULL, "\0");
 	
 	for (int i = 0; i < numTerms; i++){
-		token = strtok(str, terms[i]);
-		printf("teste\n");
-		if (token != NULL &&  str+1 == NULL){
-			return strlen(token);
+
+		if (token){
+			if(strcmp(token, terms[i]) == 0){
+				printf("token found\n");
+				return strlen(aux);
+			}
 		}
 	}
+	printf("token not found\n");
 	return strlen(str);
 }
 
 int main(void){
 	
-	char *fileName = "file.c";
+	char *fileName = malloc(sizeof(char*));
+	const char src[10] = "faaf.fes.c";
+	memcpy(fileName, src, sizeof(char)*100);
 	
-	char *term[] = {".h", ".c", ".out"};
+	char *term[] = {"c", "h", "k"};
 	
 	int res = findTerm(fileName, term, 3);
 	
-	printf("res: %d\n", res);
+	printf("res:%d\n", res);
 	
 	return 0;
 }
+
