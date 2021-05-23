5fea656fa688eea60e75468a77186af960081f7e
FranciscoFicalho97 <fbficalho@gmail.com>
2021-05-23 18:33:10 +0100

    Trab1 entrega

--- Trab1/ex2.c
+++ Trab1/ex2.c
@@ -3,17 +3,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define NUM_TERMS 20
+#define MAX_TITLE 20
 
 int findTerm( char *str, char *terms[], int numTerms ){
 	char *ret;
 
    	for (int i = 0; i < numTerms; i++){
 		ret = strrchr(str, *terms[i]);
-		printf("ret: %s\n", ret);
-		printf("str:%s\n", &str[strlen(str)]-1);
-		if (ret && (str[strlen(str)-1] == *ret)){			//VERIFICAR SE ULTIMA OCORRENCIA É O ULTIMO CHAR DA STRING 
-			printf("ret:%s\n", ret);
+		if (ret && (str[strlen(str)-strlen(terms[i])] == *ret)){			//VERIFICAR SE ULTIMA OCORRENCIA É O ULTIMO CHAR DA STRING 
 			printf("token found\n");
 			return strlen(str)-strlen(terms[i])-1;
 		}
@@ -23,12 +20,11 @@ int findTerm( char *str, char *terms[], int numTerms ){
 }
 
 int main(void){
-
 	char *fileName = malloc(sizeof(char*)+1);
-	const char src[NUM_TERMS] = "fileName.c";
-	memcpy(fileName, src, sizeof(char)*(NUM_TERMS));
+	const char src[MAX_TITLE] = "filecName.h";
+	memcpy(fileName, src, sizeof(char)*(MAX_TITLE));
 	
-	char *term[] = {"c", "h", "o"};
+	char *term[] = {"c", "h", "out"};
 	
 	int res = findTerm(fileName, term, 3);
 	
--- Trab1/ex3.c
+++ Trab1/ex3.c
@@ -17,7 +17,8 @@
 #endif
 
 #define MAX_FILE_NAME 255
-#define MAX_ITEMS 20 //65535??
+#define MAX_ITEMS 30
+#define NUM_TERMS 3
 
 enum ItemType {T_FILE, T_DIRECTORY};
 
@@ -27,18 +28,18 @@ typedef struct{
 	int term;
 } Item;
 
-char *terms[] = {"c", "h", "o"};
+char *terms[] = {"c", "h", "o"};											//array de terminações
 
 int findTerm( char *str, char *terms[], int numTerms ){
 	char *ret;
 
    	for (int i = 0; i < numTerms; i++){
 		ret = strrchr(str, *terms[i]);
-		if (ret && (str[strlen(str)-1] == *ret)){			//VERIFICAR SE ULTIMA OCORRENCIA É O ULTIMO CHAR DA STRING
-			return strlen(str)-strlen(terms[i])-1;
+		if (ret && (str[strlen(str)-strlen(terms[i])] == *ret)){			//verificar se a ocurrencia encontrada corresponde ao ultimo char da string
+			return strlen(str)-strlen(terms[i])-1;							//retornar o comprimento do nome do ficheiro subtraindo o comprimento do term encontrado e o ponto
 		}
 	}
-	return strlen(str);
+	return strlen(str);														//retornar comprimento de string
 }
 
 int loadDirList( Item list[], size_t size, char *path ){
@@ -49,7 +50,7 @@ int loadDirList( Item list[], size_t size, char *path ){
 	struct dirent * de;
 	int i = 0;
 	
-	while(i<size && ( de = readdir( d ) ) != NULL ){
+	while(i<size && ( de = readdir( d ) ) != NULL ){						//preencher cada estrutura Item
 		if(de->d_type == 4){
 			item->type = T_DIRECTORY;
 		}
@@ -57,14 +58,12 @@ int loadDirList( Item list[], size_t size, char *path ){
 			item->type = T_FILE;
 		}
 		strcpy(item->filename, de->d_name);
-		item->term = findTerm(de->d_name, terms, 3);
+		item->term = findTerm(de->d_name, terms, NUM_TERMS);
 		list[i] = *item;
-		item = malloc(sizeof(Item));
+		item = malloc(sizeof(Item));										//alocar espaço para a proxima estrutura
 		i++;			
 	}
-	
 	return i;
-	
 }
 
 int cmpFunc(const void *p1, const void *p2){
@@ -82,9 +81,9 @@ int cmpFuncAux(const void *p1, const void *p2){
     Item** aa = (Item**) p1;
     Item** bb = (Item**) p2;
 
-    int aux = strcmp( (*aa)->filename+(*aa)->term, (*bb)->filename+(*bb)->term );
+    int aux = strcmp( (*aa)->filename+(*aa)->term, (*bb)->filename+(*bb)->term );		//comparar terminacoes	
     if (aux == 0){
-		return strcmp((*aa)->filename, (*bb)->filename);
+		return strcmp((*aa)->filename, (*bb)->filename);								//comparar filename completo
 	}
     return aux;
 }
@@ -101,16 +100,15 @@ int main(){
 	int exit = 0;
 
 	char option;	
-	Item *item = malloc(sizeof(Item)*500);
-
-	char *path = ".";
+	Item *list = malloc(sizeof(Item)*MAX_ITEMS);
+	char *path = ".";														//procurar dentro da diretoria
 	
-	loadDirList(item, MAX_ITEMS, path);
+	loadDirList(list, MAX_ITEMS, path);
 	
 	Item* auxList[MAX_ITEMS];
-	
-	for(int i = 0; i < MAX_ITEMS; ++i){
-		auxList[i] = &item[i];
+
+	for(int i = 0; i < MAX_ITEMS; ++i){										//preenchimento de auxList com ponteiros para Items
+		auxList[i] = &list[i];
 	}
 	
 	printf("\t\t\tCONJUNTO DE VALORES\n\n"
@@ -122,20 +120,24 @@ int main(){
 		scanf("%c", &option);
 		switch(option){
 			case 'l':
-				sortList(item, 9);
-				for(int i = 0; i< 9; ++i){
-					printf("type: %d\n", item[i].type);
-					printf("filename: %s\n", item[i].filename);
-					printf("term: %d\n", item[i].term);
-					printf("\n");		
+				sortList(list, MAX_ITEMS);
+				for(int i = 0; i< MAX_ITEMS; ++i){
+					if (list[i].term != 0){									//verificar existencia de Item preenchido
+						printf("type: %d\n", list[i].type);
+						printf("filename: %s\n", list[i].filename);
+						printf("term: %d\n", list[i].term);
+						printf("\n");		
+					}
 				}
 				printf("\n");
 				break;
 			case 'f':
-				sortAuxList(auxList,9);
-				for(int i = 0; i< 9; ++i){
-					printf("filename: %s\n", auxList[i]->filename);
-					printf("\n");		
+				sortAuxList(auxList,MAX_ITEMS);
+				for(int i = 0; i< MAX_ITEMS; ++i){
+					if (auxList[i]->term != 0){
+						printf("filename: %s\n", auxList[i]->filename);
+						printf("\n");		
+					}
 				}
 				printf("\n");
 				break;
